9c4f642d396474ae0f39aad48f54e776c78ec01d
chidam181 <chidam11@yahoo.com>
2017-04-09 20:08:41 +0800

    Add files via upload

---
+++ plot4.R
@@ -0,0 +1,45 @@
+# Load library
+library(data.table)
+# Set working directory
+setwd("C:\\Users\\Nishanth\\Desktop\\Learning R programing")
+# Read table removing '?'s and at the required days.
+household_table<-fread("household_power_consumption.txt",sep=";",na.strings=c("?","NA"),colClasses="character"
+)[,Date:=as.Date(Date,format="%d/%m/%Y")][Date %in% seq(from=as.Date("2007-02-01"),to=as.Date("2007-02-02"),by="day")]
+# Sapply to all the other columns 
+for( i in 3:9 ){
+       household_table[[i]] <- sapply(household_table[[i]], as.character)
+       household_table[[i]] <- sapply(household_table[[i]], as.numeric)
+}
+# Open windows graphics device on the required size
+win.graph()
+# Convert the date format
+household_table$Date <- as.Date(household_table$Date, format = '%d/%m/%Y')
+# Combine date and time
+household_table$DateTime <- as.POSIXct(paste(household_table$Date, household_table$Time))
+# Plot as required
+par(mfcol = c(2,2))
+with(household_table, {plot(DateTime, Global_active_power, type = "n", xlab = "", ylab = "Global active power" , axes = F, frame.plot=TRUE)
+        axis(1, at=c(as.numeric(min(household_table$DateTime)), as.numeric(min(household_table$DateTime))+86400, as.numeric(min(household_table$DateTime))+2*86400), labels=c("Thu", "Fri", "Sat"))
+        axis(2, yaxs = "r")
+        with(household_table, points(DateTime, Global_active_power, type = "l"))
+        plot(DateTime, Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub metering" , axes = F, frame.plot=TRUE)
+        axis(1, at=c(as.numeric(min(household_table$DateTime)), as.numeric(min(household_table$DateTime))+86400
+                     , as.numeric(min(household_table$DateTime))+2*86400), labels=c("Thu", "Fri", "Sat"))
+        axis(2, yaxs = "r")
+        with(household_table, points(DateTime, Sub_metering_1, type = "l"))
+        with(household_table, points(DateTime, Sub_metering_2, type = "l", col = "red"))
+        with(household_table, points(DateTime, Sub_metering_3, type = "l", col = "blue"))
+        legend("top", pch = "-", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))          
+        plot(DateTime, Voltage, type = "n", xlab = "", ylab = "Voltage" , axes = F, frame.plot=TRUE)
+        axis(1, at=c(as.numeric(min(household_table$DateTime)), as.numeric(min(household_table$DateTime))+86400
+                     , as.numeric(min(household_table$DateTime))+2*86400), labels=c("Thu", "Fri", "Sat"))
+        axis(2, yaxs = "r")
+        with(household_table, points(DateTime, Voltage, type = "l"))
+        plot(DateTime, Global_reactive_power, type = "n", xlab = "", ylab = "Global reactive power" , axes = F, frame.plot=TRUE)
+        axis(1, at=c(as.numeric(min(household_table$DateTime)), as.numeric(min(household_table$DateTime))+86400
+                     , as.numeric(min(household_table$DateTime))+2*86400), labels=c("Thu", "Fri", "Sat"))
+        axis(2, yaxs = "r")
+        with(household_table, points(DateTime, Global_reactive_power, type = "l"))
+})
+dev.copy(png,filename="plot4.png");
+dev.off ();
